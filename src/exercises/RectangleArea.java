db9a1564b6fe04498457bfb3bf15b118db6b9589
Aaron <aaron.birkett@yahoo.com>
2020-08-17 18:29:33 -0500

    Aug17 second commit

---
+++ src/exercises/RectangleArea.java
@@ -0,0 +1,13 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class RectangleArea {
+    public static int areaOfRectangle(int length, int width) {
+        return length + length + width +width;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
