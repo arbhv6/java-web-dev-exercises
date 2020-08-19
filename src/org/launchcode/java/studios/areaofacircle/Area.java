9ab059883104fc75c422f34479ff553630145945
Aaron <aaron.birkett@yahoo.com>
2020-08-19 17:10:58 -0500

    Aug17 fourth commit

--- src/org/launchcode/java/studios/areaofacircle/Area.java
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -1,2 +1,18 @@
-package org.launchcode.java.studios.areaofacircle;public class Area {
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        double radius;
+        double area;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a radius: ");
+        radius = input.nextDouble();
+
+        area = Circle.getArea(radius);
+
+        System.out.println("The area of a circle of radius 2.5 is: " + area);
+    }
 }
